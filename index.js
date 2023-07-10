aab649fe2f54d31ead4b37935da8df99fd4890fc
hsaagal78 <hsagal78@gmail.com>
2023-07-09 23:00:08 -0400

    questions panel done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+// const { type } = require('os');
+// const { default: Choices } = require('inquirer/lib/objects/choices');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -7,12 +9,34 @@ const connection = mysql.createConnection({
     database: 'business_db'
   });
 
-  function printAllEmployees() {
-    connection.query('SELECT * FROM employees', (err, data) => {
-      if (err) return console.log(err);
+//   function printAllEmployees() {
+//     connection.query('SELECT * FROM employees', (err, data) => {
+//       if (err) return console.log(err);
   
-      console.log(data);
-      connection.end();
-    });
+//       console.log(data);
+//       connection.end();
+//     });
+//   }
+//   printAllEmployees();
+
+  function openPanel() {
+    inquirer
+    .prompt ([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee role',
+            'exit'
+        ]
+    }
+    ])
   }
-  printAllEmployees();
+  openPanel();
